674e596c109231d8ac485dc469a91c911648227d
Chaitanya <chaitanyag1718@gmail.com>
2025-12-30 16:33:15 +0530

    updated current deals

--- frontend/src/components/CurrentDeals.jsx
+++ frontend/src/components/CurrentDeals.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../api/axios";
 import { addToCart } from "../services/cartService";
-import "./css/ProductCarousel.css";
+import "../components/css/ProductCarousel.css";
 
 const CurrentDeals = () => {
   const [products, setProducts] = useState([]);
