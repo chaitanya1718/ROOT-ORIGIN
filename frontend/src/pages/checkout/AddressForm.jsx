4cf1c796e38365dccad9db89b7f2f0f8f35f9ad2
Chaitanya <chaitanyag1718@gmail.com>
2025-12-30 16:59:48 +0530

    css update

--- frontend/src/pages/checkout/AddressForm.jsx
+++ frontend/src/pages/checkout/AddressForm.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import api from "../../api/axios";
-import "../../components/css/Header.css";
+import "../../components/css/header.css";
 
 const AddressForm = ({ initialData, onSuccess, onCancel }) => {
   const [form, setForm] = useState({
